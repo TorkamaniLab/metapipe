36b451fa23718283472d8408b6f0e97b7b856772
Brian Schrader <brian@brianschrader.com>
2015-11-06 11:14:11 -0800

    Adds a more in-depth parser (not done) for CMDs.
    Also adds tests (woo!)

---
+++ test/test_parser.py
@@ -0,0 +1,61 @@
+""" Tests for the lexer and parser. """
+
+import sys, sure
+
+from metapipe import parser
+
+
+def test_split_params():
+    cmd = 'cut -hf --someflag {1,2,3||1,2||2,3,4}'
+    res = parser._split_params(cmd)
+
+    res[0].should.equal(('cut -hf --someflag {in}', ['1','2','3'], []))
+    res[1].should.equal(('cut -hf --someflag {in}', ['1','2'], []))
+    res[2].should.equal(('cut -hf --someflag {in}', ['2','3','4'], []))
+
+
+def test_parse_job():
+    cmd = 'cut -hf --someflag {1,2,3||1,2||2,3,4}'
+    jobs = []
+    files = [parser.File('file1', '1', '1'),
+            parser.File('file2', '2', '2'),
+            parser.File('file3', '3', '3'),
+            parser.File('file4', '4', '4')]
+    paths=[]
+
+    jobs = parser.parse_job(cmd, jobs, files, paths)
+
+    len(jobs).should.equal(3)
+    jobs[0].raw_cmd.should.equal('cut -hf --someflag {in}')
+
+    jobs[1].depends_on.should.contain(jobs[0])
+    jobs[2].depends_on.should.contain(jobs[0])
+    jobs[2].depends_on.should.contain(jobs[0])
+    jobs[2].depends_on.should.contain(jobs[1])
+
+
+def test_complex_job():
+    cmd = 'cut -hf --someflag {1,2,3||1,2}'
+    jobs = []
+    files = [parser.File('file1', '1', '1'),
+            parser.File('file2', '2', '2'),
+            parser.File('file3', '3', '3'),
+            parser.File('file4', '4', '4'),
+            parser.File('file5', '5', '5')]
+    paths=[]
+
+    jobs = parser.parse_job(cmd, jobs, files, paths)
+
+    cmd = 'python somescript -f {o:*.counts} {2,4||5}'
+    jobs = parser.parse_job(cmd, jobs, files, paths)
+
+
+    len(jobs).should.equal(4)
+    jobs[0].raw_cmd.should.equal('cut -hf --someflag {in}')
+    jobs[3].raw_cmd.should.equal('python somescript -f {out} {in}')
+
+    jobs[1].depends_on.should.contain(jobs[0])
+    jobs[2].depends_on.should.contain(jobs[0])
+    jobs[2].depends_on.should.contain(jobs[1])
+    len(jobs[3].depends_on).should.equal(0)
+
